67553c28b0d25e01bbd0fc1fd9a8686460062039
repoe2e <professores@e2etreinamentos.com.br>
2025-08-06 20:31:05 -0300

    inclusão do projeto

---
+++ src/test/java/dev/plataforma/tests/LoginTest.java
@@ -0,0 +1,44 @@
+package dev.plataforma.tests;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LoginTest {
+
+	@Dado("que o usuário esteja no formulário de login")
+	public void que_o_usuário_esteja_no_formulário_de_login() {
+
+	}
+
+	@Quando("enviar o email e senha")
+	public void enviar_o_email_e_senha() {
+
+	}
+
+	@Entao("acesso a tela de usuário logado")
+	public void acesso_a_tela_de_usuário_logado() {
+
+	}
+
+	@Quando("enviar o  e ")
+	public void enviar_o_e() {
+
+	}
+
+	@Entao("recebo mensagem de {string}")
+	public void recebo_mensagem_de(String string) {
+
+	}
+
+	@Quando("enviar o {string} e ")
+	public void enviar_o_e(String string) {
+
+	}
+
+	@Quando("enviar o {string} e {string}")
+	public void enviar_o_e(String string, String string2) {
+
+	}
+
+}
