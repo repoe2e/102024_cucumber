0cdb20319168f2f01c05bc3d1e4c77e314d223b6
repoe2e <professores@e2etreinamentos.com.br>
2025-08-06 22:22:58 -0300

    inclusao das validações

--- src/test/java/dev/plataforma/pages/LoginPage.java
+++ src/test/java/dev/plataforma/pages/LoginPage.java
@@ -12,6 +12,10 @@ public class LoginPage {
 	
 	
 	By btnEntrar = By.xpath("//button[@class='btn-entrar']");
+	By campoEmail = By.xpath("//input[@placeholder='Digite seu e-mail']");
+	By campoSenha = By.xpath("//input[@placeholder='Senha']");
+	By btnEntrarLogin = By.xpath("//button[@class='btn-login']");
+	By msgSessaoAtivaSim = By.xpath("//*[@class='btn-sim']");
 	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
@@ -19,11 +23,27 @@ public class LoginPage {
 	}
 	
 	public void acessarFormularioDeLogin() {
-		metodos.clicar(btnEntrar);
+		metodos.clicar(this.btnEntrar);
 	}
 	
 	public void preencherCredenciaisDeLogin(String email, String senha) {
-		
+		metodos.preencher(campoEmail, email);
+		metodos.preencher(campoSenha, senha);
 	}
+	
+	public void clicarBotaoEntrar() {
+		metodos.clicar(btnEntrarLogin);
+		try {
+			metodos.esperarElementoClicavel(msgSessaoAtivaSim, 5);
+			metodos.clicar(msgSessaoAtivaSim);
+		} catch (Exception e) {
+			System.out.println("Não apareceu a mensagem de sessão ativa.");
+		}
+	}
+	
+	public void validarPaginaLogado() {
+		metodos.validarUrl("https://plataforma.dev.e2etreinamentos.com.br/dashboard");
+	}
+	
 
 }
--- src/test/java/dev/plataforma/tests/LoginTest.java
+++ src/test/java/dev/plataforma/tests/LoginTest.java
@@ -27,32 +27,25 @@ public class LoginTest {
 
 	@Quando("enviar o email e senha")
 	public void enviar_o_email_e_senha() {
-
+		login.preencherCredenciaisDeLogin("anderson1@e2etreinamentos.com.br", "A210!dfhg254126");
+		login.clicarBotaoEntrar();
 	}
 
 	@Entao("acesso a tela de usuário logado")
 	public void acesso_a_tela_de_usuário_logado() {
-
+		login.validarPaginaLogado();
 	}
 
-	@Quando("enviar o  e ")
-	public void enviar_o_e() {
-
-	}
-
+	
 	@Entao("recebo mensagem de {string}")
 	public void recebo_mensagem_de(String string) {
 
 	}
 
-	@Quando("enviar o {string} e ")
-	public void enviar_o_e(String string) {
-
-	}
 
 	@Quando("enviar o {string} e {string}")
 	public void enviar_o_e(String string, String string2) {
-
+		login.preencherCredenciaisDeLogin(string, string2);
 	}
 
 }
